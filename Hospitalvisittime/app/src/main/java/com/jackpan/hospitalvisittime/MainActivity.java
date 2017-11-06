69c12399a8a599f588e7b3c41e14f9765edcdf33
JackPan <jheng.wei0919@gmail.com>
2017-11-06 14:42:21 +0800

    新增 ExpandableListView

--- Hospitalvisittime/app/src/main/java/com/jackpan/hospitalvisittime/MainActivity.java
+++ Hospitalvisittime/app/src/main/java/com/jackpan/hospitalvisittime/MainActivity.java
@@ -1,11 +1,24 @@
 package com.jackpan.hospitalvisittime;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jackpan.hospitalvisittime.Data.CychData;
+import com.jackpan.hospitalvisittime.Data.ExpandableListDataSource;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -13,11 +26,25 @@ import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private ArrayList<CychData> cychDataArrayList = new ArrayList<>();
-    private ProgressDialog  p;
+    private ProgressDialog p;
+    List<Map<String, String>> groups;
+    List<List<Map<String, String>>> childs;
+    private ExpandableListView mExpandableListView;
+    private  boolean deleteBool = false;
+    private String[] items;
+
+    private List<String> mExpandableListTitle;
+    private ExpandableListAdapter mExpandableListAdapter;
+
+    private Map<String, List<String>> mExpandableListData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,28 +53,75 @@ public class MainActivity extends AppCompatActivity {
         p.setMessage("122");
         p.show();
         test();
-
+        initData();
+        initExpandableListView();
 
     }
-    private void test(){
-        new Thread(){
+
+    private void initData() {
+        items = getResources().getStringArray(R.array.film_genre);
+        mExpandableListData = ExpandableListDataSource.getData(this);
+        mExpandableListTitle = new ArrayList(mExpandableListData.keySet());
+    }
+
+    private void initExpandableListView() {
+
+        final ExpandableListView elv = (ExpandableListView) findViewById(R.id.expandableListView);
+        /*限制只展開一組*/
+        elv.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+
+            }
+        });
+
+
+        mExpandableListAdapter = new CustomExpandableListAdapter(this, mExpandableListTitle, mExpandableListData);
+
+        elv.setAdapter(mExpandableListAdapter);
+        elv.setGroupIndicator(null);
+
+        elv.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+            }
+        });
+
+        elv.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                String selectedItem = ((List) (mExpandableListData.get(mExpandableListTitle.get(groupPosition))))
+                        .get(childPosition).toString();
+                Log.d(TAG, "selectedItem: " + selectedItem);
+                if (groupPosition == 1) {
+                }
+
+                return false;
+            }
+        });
+    }
+
+
+    private void test() {
+        new Thread() {
             @Override
             public void run() {
                 super.run();
                 try {
 
                     Document doc = Jsoup.connect("http://www.cych.org.tw/cych_OA/view.aspx/Dr_view.aspx").get();
-                    for (int i = 1; i < doc.select("tbody > tr").size()-1; i++) {
-                        Log.d(TAG, "run: "+  doc.select("tbody > tr").get(i).select("td").text());
-                        String array [] =doc.select("tbody > tr").get(i).select("td").text().split(" ");
-                        Log.d(TAG, "看診狀態: "+array[0].toString());
-                        Log.d(TAG, "科 別: "+array[1].toString());
-                        Log.d(TAG, "看診號: "+array[2].toString());
-                        Log.d(TAG, "最後掛號號碼: "+array[3].toString());
-                        Log.d(TAG, "現場待診人數: "+array[4].toString());
-                        Log.d(TAG, "報到狀態: "+array[5].toString());
-                        Log.d(TAG, "醫師姓名: "+array[6].toString());
-                        Log.d(TAG, "位 置: "+array[7].toString());
+                    for (int i = 1; i < doc.select("tbody > tr").size() - 1; i++) {
+                        Log.d(TAG, "run: " + doc.select("tbody > tr").get(i).select("td").text());
+                        String array[] = doc.select("tbody > tr").get(i).select("td").text().split(" ");
+                        Log.d(TAG, "看診狀態: " + array[0].toString());
+                        Log.d(TAG, "科 別: " + array[1].toString());
+                        Log.d(TAG, "看診號: " + array[2].toString());
+                        Log.d(TAG, "最後掛號號碼: " + array[3].toString());
+                        Log.d(TAG, "現場待診人數: " + array[4].toString());
+                        Log.d(TAG, "報到狀態: " + array[5].toString());
+                        Log.d(TAG, "醫師姓名: " + array[6].toString());
+                        Log.d(TAG, "位 置: " + array[7].toString());
                         CychData cychData = new CychData(array[0].toString(),
                                 array[1].toString(),
                                 array[2].toString(),
@@ -58,7 +132,6 @@ public class MainActivity extends AppCompatActivity {
                                 array[7].toString());
 
                         cychDataArrayList.add(cychData);
-                        
 
 
                     }
@@ -67,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
 
                 } catch (IOException e) {
                     e.printStackTrace();
-                    Log.d(TAG, "run: "+e.getMessage());
+                    Log.d(TAG, "run: " + e.getMessage());
                 }
 
                 runOnUiThread(new Runnable() {
@@ -81,35 +154,36 @@ public class MainActivity extends AppCompatActivity {
 
         }.start();
     }
-    private  void test2(){
-        new Thread(){
+
+    private void test2() {
+        new Thread() {
             @Override
             public void run() {
                 super.run();
                 try {
 
                     Document doc = Jsoup.connect("http://www.chimei.org.tw/left/register/progress/qprogress.asp?idept=752&host=10").get();
-                    Log.d(TAG, "run: "+doc.select("font[size=2]").text());
+                    Log.d(TAG, "run: " + doc.select("font[size=2]").text());
                     for (Element font : doc.select("font[size=2]")) {
-                        Log.d(TAG, "run: "+font.text());
-                        Log.d(TAG, "run: "+font.getElementsByTag("a").attr("href").toString());
+                        Log.d(TAG, "run: " + font.text());
+                        Log.d(TAG, "run: " + font.getElementsByTag("a").attr("href").toString());
                     }
                     for (int i = 1; i < doc.select("tr").size(); i++) {
-                        Log.d(TAG, "run: "+doc.select("tr").get(i).text());
-                        String [] array  = doc.select("tr").get(i).text().split("");
-                        Log.d(TAG, "班別: "+array[0].toString());
-                        Log.d(TAG, "科別名稱: "+array[1].toString());
-                        Log.d(TAG, "醫師姓名: "+array[2].toString());
-                        Log.d(TAG, "已掛號人數: "+array[3].toString());
-                        Log.d(TAG, "等候看診人數: "+array[4].toString());
-                        Log.d(TAG, "現在看到幾號: "+array[5].toString());
+                        Log.d(TAG, "run: " + doc.select("tr").get(i).text());
+                        String[] array = doc.select("tr").get(i).text().split("");
+                        Log.d(TAG, "班別: " + array[0].toString());
+                        Log.d(TAG, "科別名稱: " + array[1].toString());
+                        Log.d(TAG, "醫師姓名: " + array[2].toString());
+                        Log.d(TAG, "已掛號人數: " + array[3].toString());
+                        Log.d(TAG, "等候看診人數: " + array[4].toString());
+                        Log.d(TAG, "現在看到幾號: " + array[5].toString());
 
 
                     }
 
                 } catch (IOException e) {
                     e.printStackTrace();
-                    Log.d(TAG, "run: "+e.getMessage());
+                    Log.d(TAG, "run: " + e.getMessage());
                 }
 
                 runOnUiThread(new Runnable() {
