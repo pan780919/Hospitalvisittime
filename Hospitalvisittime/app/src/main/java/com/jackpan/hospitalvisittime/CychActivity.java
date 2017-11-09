279a5290ea6c7bd88cd905bd8aa826063e55072b
JackPan <jheng.wei0919@gmail.com>
2017-11-09 17:11:17 +0800

    嘉義基督 看診進度建立 ＵＩ需要修正

--- Hospitalvisittime/app/src/main/java/com/jackpan/hospitalvisittime/CychActivity.java
+++ Hospitalvisittime/app/src/main/java/com/jackpan/hospitalvisittime/CychActivity.java
@@ -1,29 +1,168 @@
 package com.jackpan.hospitalvisittime;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class CychActivity extends AppCompatActivity {
+import com.jackpan.hospitalvisittime.Data.CychData;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class CychActivity extends Activity {
+    private MyAdapter mAdapter;
+    private ArrayList<CychData> list = new ArrayList<>();
+    private ProgressDialog p;
+    private ArrayList<CychData> cychDataArrayList = new ArrayList<>();
+    private static final String TAG = "CychActivity";
+    private ListView myListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cych);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        p = new ProgressDialog(this);
+        p.setMessage("讀取中");
+        p.show();
+        test();
+        myListView = (ListView) findViewById(R.id.mylist);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+    }
+
+
+    private void test() {
+        new Thread() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                super.run();
+                try {
+
+                    Document doc = Jsoup.connect("http://www.cych.org.tw/cych_OA/view.aspx/Dr_view.aspx").get();
+                    for (int i = 1; i < doc.select("tbody > tr").size() - 1; i++) {
+                        Log.d(TAG, "run: " + doc.select("tbody > tr").get(i).select("td").text());
+                        String array[] = doc.select("tbody > tr").get(i).select("td").text().split(" ");
+                        Log.d(TAG, "看診狀態: " + array[0].toString());
+                        Log.d(TAG, "科 別: " + array[1].toString());
+                        Log.d(TAG, "看診號: " + array[2].toString());
+                        Log.d(TAG, "最後掛號號碼: " + array[3].toString());
+                        Log.d(TAG, "現場待診人數: " + array[4].toString());
+                        Log.d(TAG, "報到狀態: " + array[5].toString());
+                        Log.d(TAG, "醫師姓名: " + array[6].toString());
+                        Log.d(TAG, "位 置: " + array[7].toString());
+                        CychData cychData = new CychData(array[0].toString(),
+                                array[1].toString(),
+                                array[2].toString(),
+                                array[3].toString(),
+                                array[4].toString(),
+                                array[5].toString(),
+                                array[6].toString(),
+                                array[7].toString());
+
+                        cychDataArrayList.add(cychData);
+
+
+                    }
+                    p.dismiss();
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.d(TAG, "run: " + e.getMessage());
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter = new MyAdapter(cychDataArrayList);
+                        myListView.setAdapter(mAdapter);
+                        mAdapter.notifyDataSetChanged();
+
+                    }
+                });
             }
-        });
+
+        }.start();
+    }
+
+    public class MyAdapter extends BaseAdapter {
+        private ArrayList<CychData> mDatas;
+
+        public MyAdapter(ArrayList<CychData> datas) {
+            mDatas = datas;
+        }
+
+        public void updateData(ArrayList<CychData> datas) {
+            mDatas = datas;
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return mDatas.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mDatas.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null)
+                convertView = LayoutInflater.from(CychActivity.this).inflate(
+                        R.layout.mylayout, null);
+            CychData taipeiZoo = mDatas.get(position);
+            TextView textname = (TextView) convertView.findViewById(R.id.text1);
+            TextView list = (TextView) convertView.findViewById(R.id.text2);
+            TextView bigtext = (TextView) convertView.findViewById(R.id.text3);
+
+            TextView place = (TextView) convertView.findViewById(R.id.text4);
+            TextView time = (TextView) convertView.findViewById(R.id.text5);
+            TextView userview = (TextView) convertView.findViewById(R.id.text6);
+            TextView userlike = (TextView) convertView.findViewById(R.id.text7);
+            TextView addd = (TextView) convertView.findViewById(R.id.text8);
+
+            textname.setText(taipeiZoo.state);
+            list.setText(taipeiZoo.division);
+            bigtext.setText(taipeiZoo.number);
+            place.setText(taipeiZoo.lastNumber);
+            time.setText(taipeiZoo.watingNum);
+            userview.setText(taipeiZoo.registrationState);
+            userlike.setText(taipeiZoo.docName);
+            addd.setText(taipeiZoo.address);
+            return convertView;
+        }
+
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {   //確定按下退出鍵
+
+         this.finish();//關閉activity
+
+            return true;
+
+        }
+
+        return super.onKeyDown(keyCode, event);
+
     }
 
 }
--- Hospitalvisittime/app/src/main/java/com/jackpan/hospitalvisittime/MainActivity.java
+++ Hospitalvisittime/app/src/main/java/com/jackpan/hospitalvisittime/MainActivity.java
@@ -1,14 +1,11 @@
 package com.jackpan.hospitalvisittime;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
-
-import com.jackpan.hospitalvisittime.Data.CychData;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -21,8 +18,8 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private ArrayList<CychData> cychDataArrayList = new ArrayList<>();
-    private ProgressDialog p;
+
+
     List<Map<String, String>> groups;
     List<List<Map<String, String>>> childs;
     private ExpandableListView mExpandableListView;
@@ -38,10 +35,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        p = new ProgressDialog(this);
-        p.setMessage("讀取中");
-        p.show();
-        test();
+
         initData();
         initExpandableListView();
 
@@ -97,57 +91,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void test() {
-        new Thread() {
-            @Override
-            public void run() {
-                super.run();
-                try {
-
-                    Document doc = Jsoup.connect("http://www.cych.org.tw/cych_OA/view.aspx/Dr_view.aspx").get();
-                    for (int i = 1; i < doc.select("tbody > tr").size() - 1; i++) {
-                        Log.d(TAG, "run: " + doc.select("tbody > tr").get(i).select("td").text());
-                        String array[] = doc.select("tbody > tr").get(i).select("td").text().split(" ");
-                        Log.d(TAG, "看診狀態: " + array[0].toString());
-                        Log.d(TAG, "科 別: " + array[1].toString());
-                        Log.d(TAG, "看診號: " + array[2].toString());
-                        Log.d(TAG, "最後掛號號碼: " + array[3].toString());
-                        Log.d(TAG, "現場待診人數: " + array[4].toString());
-                        Log.d(TAG, "報到狀態: " + array[5].toString());
-                        Log.d(TAG, "醫師姓名: " + array[6].toString());
-                        Log.d(TAG, "位 置: " + array[7].toString());
-                        CychData cychData = new CychData(array[0].toString(),
-                                array[1].toString(),
-                                array[2].toString(),
-                                array[3].toString(),
-                                array[4].toString(),
-                                array[5].toString(),
-                                array[6].toString(),
-                                array[7].toString());
-
-                        cychDataArrayList.add(cychData);
-
-
-                    }
-                    p.dismiss();
-
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Log.d(TAG, "run: " + e.getMessage());
-                }
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-
-
-                    }
-                });
-            }
-
-        }.start();
-    }
 
     private void test2() {
         new Thread() {
